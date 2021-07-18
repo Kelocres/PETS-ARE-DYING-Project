a2e676c85bd8e45899670fa482e255414642e410
Kelocres <kelocres@gmail.com>
2021-07-18 12:48:09 +0200

    DecisionScript, OptionScript and VNManagerScript

---
+++ PETS ARE DYING Project/Assets/Scripts/OptionScript.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OptionScript : MonoBehaviour
+{
+    public string title;
+    public string explainResult;
+    public int imagePointer;
+    public int nextDecision;
+
+}
